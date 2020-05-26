491eb2c4b91b7817cd2b1dde235cf19c7e52e36d
minkyung <mnk918p@gmail.com>
2020-05-26 18:07:34 +0900

    settings.py for deploy

--- dw/settings.py
+++ dw/settings.py
@@ -115,6 +115,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'staticfiles'),
     os.path.join(BASE_DIR, 'upload', 'static'),
     os.path.join(BASE_DIR, 'accounts', 'static'),
 )
