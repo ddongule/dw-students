249c1e6952b261bbd6ff534097edcc1076bea50e
minkyung <mnk918p@gmail.com>
2020-05-26 17:37:31 +0900

    heroku

--- dw/settings.py
+++ dw/settings.py
@@ -115,7 +115,7 @@ STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'upload', 'static'),
     os.path.join(BASE_DIR, 'accounts', 'static'),
 )
-STATIC_ROOT = "/staticfiles/"
+STATIC_ROOT = "staticfiles"
 
 MEDIA_URL = "/student/files/"
 MEDIA_ROOT = os.path.join(BASE_DIR, 'student-uploads')
